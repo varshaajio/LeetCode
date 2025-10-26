2de8d7bf0ce6db0a8cdc97e62792a2891f35c431
Varshaa S B <varshaajio@gmail.com>
2025-10-26 14:48:48 +0000

    Sync LeetCode submission Runtime - 59 ms (84.90%), Memory - 14 MB (85.82%)

---
+++ solutions/1371-minimum-remove-to-make-valid-parentheses/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+        stack = []
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''  # mark invalid ')'
+
+        # Remove unmatched '('
+        for i in stack:
+            s[i] = ''
+
+        return ''.join(s)
+
