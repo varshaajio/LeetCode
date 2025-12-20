7d902deb1d21a0daa602a07260474ae44a6c7981
Varshaa S B <varshaajio@gmail.com>
2025-12-20 13:15:32 +0000

    Sync LeetCode submission Runtime - 31 ms (93.76%), Memory - 18.1 MB (100.00%)

---
+++ solutions/0981-delete-columns-to-make-sorted/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        counter = 0
+
+        for i in zip(*strs):
+            if list(i)!=sorted(i):
+                counter+=1
+
+        return counter
