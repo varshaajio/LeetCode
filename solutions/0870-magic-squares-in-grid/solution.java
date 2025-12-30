58770cf4129ddc613fb0ba8508fdef2aa2326386
Varshaa S B <varshaajio@gmail.com>
2025-12-30 15:57:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.2 MB (69.42%)

---
+++ solutions/0870-magic-squares-in-grid/solution.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        // Loop over all possible 3x3 subgrids
+        for (int i = 0; i <= rows - 3; i++) {
+            for (int j = 0; j <= cols - 3; j++) {
+                if (isMagic(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean isMagic(int[][] grid, int r, int c) {
+        // Center must be 5
+        if (grid[r + 1][c + 1] != 5) return false;
+
+        boolean[] seen = new boolean[10];
+
+        // Check numbers 1–9 and uniqueness
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int val = grid[i][j];
+                if (val < 1 || val > 9 || seen[val]) {
+                    return false;
+                }
+                seen[val] = true;
+            }
+        }
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2] != 15)
+                return false;
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (grid[r][c + j] + grid[r + 1][c + j] + grid[r + 2][c + j] != 15)
+                return false;
+        }
+
+        // Check diagonals
+        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != 15)
+            return false;
+
+        if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != 15)
+            return false;
+
+        return true;
+    }
+}
+
