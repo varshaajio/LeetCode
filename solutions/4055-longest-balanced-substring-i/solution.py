354a167f463d9e6b902195cb8c08203c863e1979
Varshaa S B <varshaajio@gmail.com>
2026-02-12 13:32:26 +0000

    Sync LeetCode submission Runtime - 2501 ms (61.86%), Memory - 19.3 MB (26.57%)

--- solutions/4055-longest-balanced-substring-i/solution.py
+++ solutions/4055-longest-balanced-substring-i/solution.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def longestBalanced(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def longestBalanced(self, s: str) -> int:
         n = len(s)
-        o = 0
+        max_len = 0
+        
         for i in range(n):
-            freq = {}
+            # Frequency map for the current window starting at i
+            counts = {}
             for j in range(i, n):
-                c = s[j]
-                freq[c] = freq.get(c, 0) + 1
-                if len(freq) > 1:
-                    vals = list(freq.values())
-                    if max(vals) == min(vals):
-                        o = max(o, j - i + 1)
-                else:
-                    # only one unique char so far
-                    o = max(o, j - i + 1)
-        return o
-
+                char = s[j]
+                counts[char] = counts.get(char, 0) + 1
+                
+                # Get all frequencies currently in the map
+                freq_values = counts.values()
+                
+                # Check if all frequencies are the same
+                # Using a set is a quick way to see if all values are identical
+                if len(set(freq_values)) == 1:
+                    max_len = max(max_len, j - i + 1)
+                    
+        return max_len
